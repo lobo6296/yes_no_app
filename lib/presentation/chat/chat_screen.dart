79c86f5eadfc887943e90be0339615a0e8426cd7
Jose Zamora <azamor4@gmail.com>
2024-11-06 14:05:45 -0600

    se avanza con la pantalla

---
+++ lib/presentation/chat/chat_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://www.medespoir.fr/wp-content/uploads/2019/05/jennifer-aniston.jpg'),
+          ),
+        ),
+        title: const Text('Mi amor '),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Text('Indice: $index');
+              },
+            )),
+            Text('Mundo')
+          ],
+        ),
+      ),
+    );
+  }
+}
